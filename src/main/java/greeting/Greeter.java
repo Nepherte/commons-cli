166c9d69b93f6a913859314ad9f8982f85727b3f
Bart Verhoeven <bartverhoeven@live.com>
2018-05-31 20:46:57 +0800

    Ant build.

---
+++ src/main/java/greeting/Greeter.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2012-2018 Bart Verhoeven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package greeting;
+
+/**
+ * Greeter that prints welcome messages.
+ */
+public class Greeter {
+
+  /**
+   * Says hello to a user.
+   *
+   * @param user the user
+   * @return a greeting
+   */
+  public String sayHello(String user) {
+    return String.format("Hello, %1$s", user);
+  }
+}
