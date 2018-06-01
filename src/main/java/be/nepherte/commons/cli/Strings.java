55296059da7edeebac1bb9b4772d38e030428c35
Bart Verhoeven <bartverhoeven@live.com>
2018-06-01 23:29:19 +0800

    Strings.

---
+++ src/main/java/be/nepherte/commons/cli/Strings.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2012-2018 Bart Verhoeven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.nepherte.commons.cli;
+
+import java.util.Arrays;
+
+/**
+ * Utility class for {@code String}-related functionality.
+ */
+final class Strings {
+
+  /**
+   * Creates a new instance.
+   */
+  private Strings() {
+    // Hide constructor.
+  }
+
+  /**
+   * Converts whitespace strings to {@code null}.
+   *
+   * @param string the string to convert
+   * @return {@code null} if whitespace-only
+   */
+  public static String whitespaceToNull(String string) {
+    return isNullOrWhitespace(string) ? null : string;
+  }
+
+  /**
+   * Converts empty strings to {@code null}.
+   *
+   * @param string the string to convert
+   * @return {@code null} if empty
+   */
+  public static String emptyToNull(String string) {
+    return isNullOrEmpty(string) ? null : string;
+  }
+
+  /**
+   * Converts {@code null} to empty strings.
+   *
+   * @param string the string to convert
+   * @return empty if {@code null}
+   */
+  public static String nullToEmpty(String string) {
+    return isNullOrEmpty(string) ? "" : string;
+  }
+
+  /**
+   * Returns true if the string is {@code null} or empty.
+   *
+   * @param string the string to test
+   * @return true if {@code null} or empty
+   */
+  public static boolean isNullOrEmpty(String string) {
+    return string == null || string.isEmpty();
+  }
+
+  /**
+   * Indicates if the string is {@code null} or whitespace.
+   *
+   * @param string the string to test
+   * @return true if {@code null} or whitespace
+   */
+  public static boolean isNullOrWhitespace(String string) {
+    return string == null || string.trim().isEmpty();
+  }
+
+  /**
+   * Repeats a character by a given amount.
+   *
+   * @param c the character to repeat
+   * @param n the amount to repeat it
+   * @return a character repeated by the given amount
+   * @throws IllegalArgumentException {@code n} is negative
+   */
+  public static String repeat(char c, int n) {
+    if (n < 0) {
+      throw new IllegalArgumentException(
+        "Cannot repeat character [" + c + "] by negative amount [" + n + "]"
+      );
+    }
+
+    char[] data = new char[n];
+    Arrays.fill(data, c);
+    return String.valueOf(data);
+  }
+}
---
+++ src/test/java/be/nepherte/commons/cli/StringsTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2012-2018 Bart Verhoeven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.nepherte.commons.cli;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test that covers {@link Strings}.
+ */
+public final class StringsTest {
+
+  @Test
+  public void isNullOrEmpty() {
+    assertThat(Strings.isNullOrEmpty(null), is(true));
+    assertThat(Strings.isNullOrEmpty(""), is(true));
+    assertThat(Strings.isNullOrEmpty(" "), is(false));
+    assertThat(Strings.isNullOrEmpty("\t"), is(false));
+    assertThat(Strings.isNullOrEmpty("a"), is(false));
+  }
+
+  @Test
+  public void nullToEmpty() {
+    assertThat(Strings.nullToEmpty(null), is(""));
+    assertThat(Strings.nullToEmpty(""), is(""));
+    assertThat(Strings.nullToEmpty(" "), is(" "));
+    assertThat(Strings.nullToEmpty("\t"), is("\t"));
+    assertThat(Strings.nullToEmpty("a"), is("a"));
+  }
+
+  @Test
+  public void emptyToNull() {
+    assertThat(Strings.emptyToNull(null), nullValue());
+    assertThat(Strings.emptyToNull(""), nullValue());
+    assertThat(Strings.emptyToNull(" "), is(" "));
+    assertThat(Strings.emptyToNull("\t"), is("\t"));
+    assertThat(Strings.emptyToNull("a"), is("a"));
+  }
+
+  @Test
+  public void isNullOrWhitespace() {
+    assertThat(Strings.isNullOrWhitespace(null), is(true));
+    assertThat(Strings.isNullOrWhitespace(""), is(true));
+    assertThat(Strings.isNullOrWhitespace("  "), is(true));
+    assertThat(Strings.isNullOrWhitespace("\t"), is(true));
+    assertThat(Strings.isNullOrWhitespace("a"), is(false));
+  }
+
+  @Test
+  public void whitespaceToNull() {
+    assertThat(Strings.whitespaceToNull(null), nullValue());
+    assertThat(Strings.whitespaceToNull(""), nullValue());
+    assertThat(Strings.whitespaceToNull("  "), nullValue());
+    assertThat(Strings.whitespaceToNull("\t"), nullValue());
+    assertThat(Strings.whitespaceToNull("a"), is("a"));
+  }
+
+  @Test
+  public void repeat() {
+    String result = Strings.repeat('a', 6);
+    assertThat(result, is("aaaaaa"));
+  }
+
+  @Test
+  public void repeatZeroTimes() {
+    String result = Strings.repeat('a', 0);
+    assertThat(result.isEmpty(), is(true));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void repeatNegativeAmount() {
+    Strings.repeat('a', -1);
+  }
+}
