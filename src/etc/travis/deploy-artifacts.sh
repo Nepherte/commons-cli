3dd1ff4f46ca962c6754c248fa2ce56e7641fc90
Bart Verhoeven <bartverhoeven@live.com>
2020-03-07 19:30:12 +0800

    Switch to Maven.
    Signed-off-by: Bart Verhoeven <bartverhoeven@live.com>

--- src/etc/travis/deploy-artifacts.sh
+++ src/etc/travis/deploy-artifacts.sh
@@ -3,18 +3,18 @@
 # This script compiles, packages and finally deploys the project's artifacts to
 # the Ivy Repository. Tests are assumed to have run in a previous build stage.
 
-# The location in which Ant is installed.
-ANT_DIR="${TRAVIS_BUILD_DIR}/bin"
-
 # The location in which the build file resides.
-BUILD_FILE="${TRAVIS_BUILD_DIR}/src/etc/ant/build.xml"
+BUILD_FILE="${TRAVIS_BUILD_DIR}/pom.xml"
 
-function deploy_to_ivy() {
-    "${ANT_DIR}/ant" -Dssh.key.password=${SSH_KEY_PASSWORD} -Dtest.skip=true -f "${BUILD_FILE}" publish
+# The location in which the settings file resides.
+SETTINGS_FILE="${TRAVIS_BUILD_DIR}/src/etc/maven/settings.xml"
+
+function deploy_to_github() {
+  mvn --settings "${SETTINGS_FILE}" --file "${BUILD_FILE}" -DskipTests deploy
 }
 
 # -e: exit script immediately when a command returns a non-zero value.
 set -e
 
-deploy_to_ivy
+deploy_to_github
 
--- src/etc/travis/run-unit-tests.sh
+++ src/etc/travis/run-unit-tests.sh
@@ -3,14 +3,14 @@
 ## This script runs the unit tests of this project. When a test fails, this
 ## results in an overall build failure on Travis CI as well.
 
-# The location in which Ant is installed.
-ANT_DIR="${TRAVIS_BUILD_DIR}/bin"
-
 # The location in which the build file resides.
-BUILD_FILE="${TRAVIS_BUILD_DIR}/src/etc/ant/build.xml"
+BUILD_FILE="${TRAVIS_BUILD_DIR}/pom.xml"
+
+# The location in which the settings file resides.
+SETTINGS_FILE="${TRAVIS_BUILD_DIR}/src/etc/maven/settings.xml"
 
 function run_unit_tests() {
-    "${ANT_DIR}/ant" -Dssh.key.password=${SSH_KEY_PASSWORD} -f "${BUILD_FILE}" test
+  mvn --settings "${SETTINGS_FILE}" --file "${BUILD_FILE}" test
 }
 
 # -e: exit script immediately when a command returns a non-zero value.
